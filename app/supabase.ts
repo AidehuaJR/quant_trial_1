b29d554bf62d50efee9c08f30701aedebaecdaf6
AidehuaJR <kunheeedward@gmail.com>
2026-07-26 18:29:55 +0900

    Fix Supabase project URL

--- app/supabase.ts
+++ app/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ??
-  "https://ywljkygtretrlrwbkqi.supabase.co";
+  "https://ywljkygtretlrlrwbkqi.supabase.co";
 
 const supabasePublishableKey =
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
